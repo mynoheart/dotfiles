260ff5aec26bab71d3835e269ff9914c59e56ec4
Tarasov Sergey <tarasov@frankrg.com>
2016-06-11 10:59:40 +0300

    fish: removed envs

--- fish/config.fish
+++ fish/config.fish
@@ -3,11 +3,6 @@
 set -x LANG "en_US.UTF-8"
 
 set fish_path $HOME/.oh-my-fish
-set -x OCI_HOME /opt/instantclient_12_1
-set -x OCI_LIB_DIR $OCI_HOME
-set -x OCI_INCLUDE_DIR $OCI_HOME/sdk/include
-set -x OCI_INC_DIR $OCI_HOME/sdk/include
-set -x OCI_VERSION 12
 
 # Path to Oh My Fish install.
 set -gx OMF_PATH "/home/mynoheart/.local/share/omf"
@@ -17,8 +12,6 @@ set -gx OMF_PATH "/home/mynoheart/.local/share/omf"
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
-
-alias cdcur 'cd (cat ~/.cwd)'
 
 #git aliases
 alias ga 'git add'
@@ -38,9 +31,3 @@ set -x PKG_CONFIG_PATH $GOPATH/pkg-configs
 if test -e ~/.config/fish/config-local.fish
   source ~/.config/fish/config-local.fish
 end
-
-# set -x ORACLE_HOME /usr/lib/oracle/product/11.2.0/xe
-# set -x TNS_ADMIN $HOME/oracle-home/
-
-set -x NLS_LANG 'AMERICAN_AMERICA.AL32UTF8'
-set -x LD_LIBRARY_PATH /opt/instantclient_12_1/
