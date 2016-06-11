e3919ced1245d16e68e5fc8c208ffcf46ccdbcf4
Tarasov Sergey <mynoheart@gmail.com>
2016-06-11 10:32:12 +0300

    oh-my-fish

--- fish/config.fish
+++ fish/config.fish
@@ -8,30 +8,16 @@ set -x OCI_LIB_DIR $OCI_HOME
 set -x OCI_INCLUDE_DIR $OCI_HOME/sdk/include
 set -x OCI_INC_DIR $OCI_HOME/sdk/include
 set -x OCI_VERSION 12
-# Theme
-# set fish_theme agnoster
-set fish_theme bobthefish
-# set fish_theme coffeeandcode
-# set fish_theme cor
-# set fish_theme fishface
-# set fish_theme idan
-# set fish_theme jacaetevha
-# set fish_theme krisleech
-# set fish_theme l
-# set fish_theme numist
-# set fish_theme robbyrussell
-# set fish_theme uggedal
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
 
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/home/mynoheart/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/home/mynoheart/.config/omf"
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-if test -f $HOME/.cwd;cd (cat $HOME/.cwd); end
-# set PATH $PATH (ruby -rubygems -e 'puts Gem.user_dir')/bin
+source $OMF_PATH/init.fish
+
 alias cdcur 'cd (cat ~/.cwd)'
 
 #git aliases
