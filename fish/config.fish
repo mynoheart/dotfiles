67663ee0ff289fd7a52c1276030b4f18d2ed8844
Tarasov Sergey <tarasov@frankrg.com>
2018-11-22 14:58:49 +0300

    Убраны лишние папки для PATH, поправлена работа с русскими путями для Гита

--- fish/config.fish
+++ fish/config.fish
@@ -8,10 +8,14 @@ set fish_path $HOME/.oh-my-fish
 set -gx OMF_PATH "/home/mynoheart/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/mynoheart/.config/omf"
+set -gx OMF_CONFIG "/home/mynoheart/.config/omf"
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
+
+# function fish_prompt
+#     ~/.powerline/powerline-go -error $status -shell bare -modules cwd,docker,exit,git,host,jobs,perms,root,ssh,venv -cwd-max-depth 10 -newline
+# end
 
 #git aliases
 alias ga 'git add'
@@ -25,7 +29,6 @@ alias gd 'git diff'
 #npm aliases
 alias npm-exec 'set PATH $PATH (npm bin)'
 
-set -x PATH $PATH ~/.gotools/
 set -x PKG_CONFIG_PATH $GOPATH/pkg-configs
 
 if test -e ~/.config/fish/config-local.fish
@@ -38,5 +41,6 @@ alias vimnp 'vim --noplugin'
 alias vima 'vim -u ~/.vimrca --cmd \'set runtimepath=~/.vima,$VIM/vimfiles,$VIMRUNTIME,$VIM/vimfiles/after,$HOME/.vim/after\''
 
 set -x TERM xterm-256color
-
-set PATH $PATH (ruby -rubygems -e 'puts Gem.user_dir')/bin
+# set -x PATH (ruby -rubygems -e 'puts Gem.user_dir')/bin $PATH
+# set -x GEM_HOME (ruby -e 'print Gem.user_dir')
+# rvm default
